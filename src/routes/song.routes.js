bbfe2c78751a3757539b089a393a1d7fbd37f93c
Joselyn Pilco <pasante.it@sime.com.ec>
2025-07-27 16:28:08 -0500

    changes

--- src/routes/song.routes.js
+++ src/routes/song.routes.js
@@ -6,16 +6,16 @@ const songController = require('../controllers/song.controller');
 // GET /songs - Obtener todas las canciones
 router.get('/', songController.getAll);
 
-// GET /songs/:id - Obtener una canción por ID (RESTful)
+// GET /songs/:id - Obtener una canción por ID 
 router.get('/:id', songController.getById);
 
 // POST /songs - Crear una nueva canción
 router.post('/', songController.create);
 
-// PUT /songs/:id - Actualizar una canción existente (RESTful)
+// PUT /songs/:id - Actualizar una canción existente 
 router.put('/:id', songController.update);
 
-// DELETE /songs/:id - Eliminar una canción (RESTful)
+// DELETE /songs/:id - Eliminar una canción
 router.delete('/:id', songController.remove);
 
 module.exports = router;
