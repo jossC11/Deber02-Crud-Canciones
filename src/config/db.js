4927c70d917c6daac1ade5dc568e16b3adad08e2
Joselyn Pilco <pasante.it@sime.com.ec>
2025-07-27 13:30:02 -0500

    changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,9 @@
-const sql = require('mssql');
+require('dotenv').config();
+const { Pool } = require('pg');
 
-const config = {
-    user: 'usr_polimusic_gr1',
-    password: 'Politecnica1',
-    server: 'localhost',
-    database: 'BDD_PoliMusic_GR1',
-    options: {
-        encrypt: false, // Cambia a true si usas Azure
-        trustServerCertificate: true // Para desarrollo local
-    }
-};
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Importante para Railway
+});
 
-const poolPromise = new sql.ConnectionPool(config)
-    .connect()
-    .then(pool => {
-        console.log('Conectado a SQL Server');
-        return pool;
-    })
-    .catch(err => console.log('Error de conexión a la base de datos:', err));
-
-module.exports = {
-    sql, poolPromise
-};
+module.exports = { pool };
